eb811b72b474d43ebc42a2390a2e376c738f46ac
Himaja Kanneganti <Himaja Kanneganti@DESKTOP-N3G9I18>
2020-01-27 11:07:09 +0530

    AutoIt Example

--- Github/FirstJavaProject/src/secondJavaPackage/AutoItEx.java
+++ Github/FirstJavaProject/src/secondJavaPackage/AutoItEx.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AutoItEx 
 {
 
-	public static void main(String[] args) throws Exception
+	public static void main(String[] args) 
 	{
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://www.convertfiles.com/");
